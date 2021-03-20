9934c0eea40ebaae3eb91e87ed8b668d0f9487a8
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 15:31:04 -0700

    Add counter state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <div className="App">
       <h1>This is counter app</h1>
+      <div id="counter-value">{counter}</div>
       <button id="increment-btn">Increment</button>
     </div>
   );
