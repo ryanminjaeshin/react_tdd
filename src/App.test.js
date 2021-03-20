5d8f2e7d5d7b7fb0870e82934cd3bec7cb49bb57
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 15:16:00 -0700

    Add wrapper for App component

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,13 @@ import App from './App';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 configure({ adapter: new Adapter() });
 
 describe('Counter Testing', () => {
   test('renders the title of counter', () => {
+    // shallow creates instances of a component.
+    // it only renders App component not childs.
+    const wrapper = shallow(<App />);
     render(<App />);
     const linkElement = screen.getByText('This is counter app');
     expect(linkElement).toBeInTheDocument();
