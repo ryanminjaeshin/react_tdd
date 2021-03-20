0f60a4f21f7afad9dc84d46256e551cf39a47d13
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 15:38:41 -0700

    add decrement button

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h1>This is counter app</h1>
       <div id="counter-value">{counter}</div>
       <button id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button id="decrement-btn" onClick={() => setCounter(counter - 1)}>decrement</button>
     </div>
   );
 }
