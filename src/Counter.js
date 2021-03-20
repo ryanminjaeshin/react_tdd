dd552a7e0fe3096aef6b84fcda9e3c6375664a8d
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 16:02:39 -0700

    Done

---
+++ src/Counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div>
+      <h1>This is counter app</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button id="decrement-btn" onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </div>
+  )
+}
