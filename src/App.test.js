eee0be2c0ba5192322d44a7fe56ac249c2854576
Ryan Shin <ryanminjaeshin@gmail.com>
2021-03-20 15:13:49 -0700

    import enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('Counter Testing', () => {
+  test('renders the title of counter', () => {
+    render(<App />);
+    const linkElement = screen.getByText('This is counter app');
+    expect(linkElement).toBeInTheDocument();
+  });
+})
+
